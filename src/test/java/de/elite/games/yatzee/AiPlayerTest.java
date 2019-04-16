7fbb40e77c3472cf6f91c2f609ce8eb252a79926
martin.frank <martin.frank.private@gmail.com>
2019-04-16 13:46:36 +0200

    added Test Stub for PayerAI

---
+++ src/test/java/de/elite/games/yatzee/AiPlayerTest.java
@@ -0,0 +1,13 @@
+package de.elite.games.yatzee;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AiPlayerTest {
+
+    @Test
+    public void testStrategy() {
+        //FIXME
+        Assert.assertTrue(true);
+    }
+}
