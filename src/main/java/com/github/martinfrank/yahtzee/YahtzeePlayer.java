13cabec280de7886db9f634f0cf348cedf3c924b
martin.frank <martin.frank.private@gmail.com>
2019-05-20 12:11:48 +0200

    refactoring removed code smells

--- src/main/java/com/github/martinfrank/yahtzee/YahtzeePlayer.java
+++ src/main/java/com/github/martinfrank/yahtzee/YahtzeePlayer.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 public class YahtzeePlayer extends BasePlayer<YahtzeeGame> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(YahtzeePlayer.class);
-//    private Strategy currentStrategy;
 
 
     YahtzeePlayer(String name, int color, boolean isHuman) {
@@ -46,52 +45,4 @@ public class YahtzeePlayer extends BasePlayer<YahtzeeGame> {
         yahtzeeGame.startPlayersTurn();
     }
 
-//    private void executeRoll(boolean setKeeping) {
-//        YahtzeeGame yatzeeGame = getBoardGame();
-//        StrategyAdviser strategyAdviser = new StrategyAdviser(yatzeeGame);
-//        yatzeeGame.roll();
-//        LOGGER.debug("roll: {}", yatzeeGame.getRoll());
-//        currentStrategy = strategyAdviser.getAdvise();
-//        LOGGER.debug("best strategy seems to be {}", currentStrategy);
-//
-//        if(setKeeping){
-//            yatzeeGame.setKeepings(currentStrategy.getKeeping());
-//        }
-//
-//    }
-
-//    private Keeping getKeepsForStrategyAndRoll(Strategy strategy, RollAnalyze rollAnalyze) {
-//        switch (strategy) {
-//            case ROLL_FOR_IDENTICAL: return applyStrategyThreeOrFourOfAKind(rollAnalyze);
-//            case ROLL_FOR_FIVE_AND_SIX: return applyStrategyRollForFiveAndSix(rollAnalyze);
-//            case ROLL_FOR_SIX: return applyStrategyRollForSix(rollAnalyze);
-//            case ROLL_FOR_FIVE: return applyStrategyRollForFive(rollAnalyze);
-//            default: return applyStrategyReRollAll();
-//        }
-//    }
-//
-//    private Keeping applyStrategyRollForFiveAndSix(RollAnalyze rollAnalyze) {
-//        return applyStrategyRollForN(rollAnalyze, 5, 6);
-//    }
-//
-//    private Keeping applyStrategyRollForSix(RollAnalyze rollAnalyze) {
-//        return applyStrategyRollForN(rollAnalyze, 5);
-//    }
-//
-//    private Keeping applyStrategyRollForFive(RollAnalyze rollAnalyze) {
-//        return applyStrategyRollForN(rollAnalyze, 5);
-//    }
-//
-//    private Keeping applyStrategyRollForN(RollAnalyze rollAnalyze, int... eyes) {
-//        return rollAnalyze.getKeepingFor(eyes);
-//    }
-//
-//    private Keeping applyStrategyReRollAll() {
-//        return new Keeping(Collections.emptySet());
-//    }
-//
-//    private Keeping applyStrategyThreeOrFourOfAKind(RollAnalyze rollAnalyze) {
-//        int eye = rollAnalyze.getHighestEyeOfIdenticals();
-//        return rollAnalyze.getKeepingFor(eye);
-//    }
 }
